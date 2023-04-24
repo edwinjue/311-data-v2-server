f101493c5e91251567800d682afa3b232ed7194a
Edwin J <hungrylulu8@gmail.com>
2023-04-23 18:34:26 -0700

    initial commit

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+export default function Index() {
+  return (
+    <>
+      <p>311-data-v2 API Server at your service!</p><br/>
+      <br/>
+      Try these endpoints:<br/>
+      <ol>
+        <li>
+          <a href="/geojson/geocode?latitude=34.051511&longitude=-118.251459">/geojson/geocode?latitude=34.051511&longitude=-118.251459</a>
+        </li>
+      </ol>
+    </>
+  )
+}
