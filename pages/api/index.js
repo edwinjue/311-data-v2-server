a1ffdcf1b538987bdf6ab62d8a49acc4d91bc009
Edwin J <hungrylulu8@gmail.com>
2023-04-23 20:37:52 -0700

    trying to import runMiddleware method using alias from utils/index

--- pages/api/index.js
+++ pages/api/index.js
@@ -1,26 +1,13 @@
 const app = require('express')();
 const { v4 } = require('uuid');
 const Cors = require('cors');
+const { runMiddleware } = require('@utils/index');
 
 // Initializing the cors middleware
 // You can read more about the available options here: https://github.com/expressjs/cors#configuration-options
 const cors = Cors({
   methods: ['POST', 'GET', 'HEAD'],
 });
-
-// Helper method to wait for a middleware to execute before continuing
-// And to throw an error when an error happens in a middleware
-async function runMiddleware(req, res, fn) {
-  return new Promise((resolve, reject) => {
-    fn(req, res, (result) => {
-      if (result instanceof Error) {
-        return reject(result);
-      }
-
-      return resolve(result);
-    });
-  });
-}
 
 // API examples
 app.get('/api', (req, res) => {
@@ -36,12 +23,12 @@ app.get('/api/item/:slug', (req, res) => {
 });
 
 // CORS example
-app.get('/api/cors', async (req,res)=>{
+app.get('/api/cors', async (req, res) => {
   // Run the middleware
   await runMiddleware(req, res, cors);
 
   // Rest of the API logic
   res.json({ message: 'Hello Everyone!' });
-})
+});
 
 module.exports = app;
