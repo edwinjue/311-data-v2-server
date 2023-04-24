1a31e9cf14c091e93a2bced5ea8aa506371e1715
Edwin J <hungrylulu8@gmail.com>
2023-04-23 18:41:42 -0700

    added cors

--- pages/api/index.js
+++ pages/api/index.js
@@ -1,6 +1,27 @@
 const app = require('express')();
 const { v4 } = require('uuid');
 
+// Initializing the cors middleware
+// You can read more about the available options here: https://github.com/expressjs/cors#configuration-options
+const cors = Cors({
+  methods: ['POST', 'GET', 'HEAD'],
+})
+
+// Helper method to wait for a middleware to execute before continuing
+// And to throw an error when an error happens in a middleware
+function runMiddleware(req, res) {
+  return new Promise((resolve, reject) => {
+    (req, res, (result) => {
+      if (result instanceof Error) {
+        return reject(result)
+      }
+
+      return resolve(result)
+    })
+  })
+}
+
+// API examples
 app.get('/api', (req, res) => {
   const path = `/api/item/${v4()}`;
   res.setHeader('Content-Type', 'text/html');
@@ -12,5 +33,14 @@ app.get('/api/item/:slug', (req, res) => {
   const { slug } = req.params;
   res.end(`Item: ${slug}`);
 });
+
+// CORS example
+app.get('/cors', async (req,res)=>{
+  // Run the middleware
+  await runMiddleware(req, res, cors)
+
+  // Rest of the API logic
+  res.json({ message: 'Hello Everyone!' })
+})
 
 module.exports = app;
